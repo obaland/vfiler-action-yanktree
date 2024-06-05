bd5ced7bbb37e643f8c1797c2209e9cddb9e68d6
th.obaland@gmail.com <th.obaland@gmail.com>
2024-06-05 23:43:36 +0900

    add: yank directory tree.

--- lua/vfiler/actions/yanktree.lua
+++ lua/vfiler/actions/yanktree.lua
@@ -6,39 +6,44 @@ local M = {}
 
 -- Default configs
 M.configs = {
-  directories_only = false,
-  options_extened = '',
+  ignore_directories = {},
+  options = '',
 }
 
-local function system(dirpath)
+local function system(dirpath, directory_only)
   local commands = { 'tree' }
   if core.is_windows then
-    if not M.configs.directories_only then
+    if not directory_only then
       table.insert(commands, '/f')
     end
   else
-    if M.configs.directories_only then
+    if directory_only then
       table.insert(commands, '-d')
     end
+    -- TODO:
+    if #M.configs.ignore_directories > 0 then
+      local ignores = M.configs.ignore_directories
+      table.insert(commands, ("-I '%s'"):format(table.concat(ignores, '|')))
+    end
   end
-  if #M.configs.options_extened > 0 then
-    table.insert(commands, M.configs.options_extened)
+  if #M.configs.options > 0 then
+    table.insert(commands, M.configs.options)
   end
   table.insert(commands, ('"%s"'):format(dirpath))
   return core.system(table.concat(commands, ' '))
 end
 
-function M.yank_tree_root(vfiler, context, view)
+local function yank_tree_root(context, view, directory_only)
   if vim.fn.executable('tree') ~= 1 then
     core.message.warning("The 'tree' command is requrired to run this.")
     return
   end
   local path = context.root.path
-  clipboard.yank(system(path))
+  clipboard.yank(system(path, directory_only))
   core.message.info('Yanked "%s" tree', path)
 end
 
-function M.yank_tree(vfiler, context, view)
+local function yank_tree(context, view, directory_only)
   if vim.fn.executable('tree') ~= 1 then
     core.message.warning("The 'tree' command is requrired to run this.")
     return
@@ -51,8 +56,24 @@ function M.yank_tree(vfiler, context, view)
   if not core.path.is_directory(path) then
     path = item.parent.path
   end
-  clipboard.yank(system(path))
+  clipboard.yank(system(path, directory_only))
   core.message.info('Yanked "%s" tree', path)
+end
+
+function M.yank_tree_root(vfiler, context, view)
+  yank_tree_root(context, view, false)
+end
+
+function M.yank_tree(vfiler, context, view)
+  yank_tree(context, view, false)
+end
+
+function M.yank_directory_tree_root(vfiler, context, view)
+  yank_tree_root(context, view, true)
+end
+
+function M.yank_directory_tree(vfiler, context, view)
+  yank_tree(context, view, true)
 end
 
 --- Setup configs
